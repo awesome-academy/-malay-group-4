d4148eed5b654a25ab6754390ba85e80c855c8d5
Moonforeva <moonforeva@gmail.com>
2020-09-28 17:54:15 +0800

    Design Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title page_title
+    base_title = "COURSE REGISTRATION SYSTEM"
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
