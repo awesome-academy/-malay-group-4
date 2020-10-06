ab022d5b83211ebce199863b5f88d6ff2949b38c
nuriman1 <imanrasid98@gmail.com>
2020-10-06 14:57:33 +0800

    password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_090345) do
+ActiveRecord::Schema.define(version: 2020_10_06_081317) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_10_05_090345) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.boolean "admin", default: false
+    t.string "reset_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
